c2b682f36be26e7b15a34899ac5417f95ea45b02
Carlos Jorge Cary Huanca <gary7412@hotmail.com>
2020-05-15 19:40:48 -0400

    fix dirs

--- build/routes/FormTaskRoutes.js
+++ build/routes/FormTaskRoutes.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const FormTaskController_1 = require("../controllers/FormTaskController");
+const FormTaskController_1 = require("../controllers/formTaskController");
 class FormTaskRoutes {
     constructor() {
         this.router = express_1.Router();
